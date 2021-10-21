88ddeecbfbc84c3466feb1cdf45eea0aec719a70
AdityaTeltia <teltia.aditya22@gmail.com>
2021-10-21 14:18:21 -0700

    Initial Commit
    Signed-off-by: AdityaTeltia <teltia.aditya22@gmail.com>

---
+++ pages/predict.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function predict() {
+    return (
+        <div>
+            <h1>CNN Model to Predict Quality of Crops </h1>
+        </div>
+    )
+}
+
+export default predict
+
